8b7a15a063834795448ec2406aa4d042f83da398
Durga Kundu <durgakundu888@gmail.com>
2023-11-21 07:33:57 -0800

    Update form.test.js

--- tests/form.test.js
+++ tests/form.test.js
@@ -1,29 +1,55 @@
 // addinvestor.test.js
-// Import the addInvestor function
+// Import the functions from the correct paths
 const { addInvestor, deleteInvestor, validateAndConvert } = require('../scripts/index.js');
 const { prorate } = require('../scripts/prorate.js');
-console.log(prorate);
+
 // Mock the alert function to capture alerts during tests
 global.alert = jest.fn();
 
-// Mock the necessary DOM elements
-document.body.innerHTML = `
-    <div id="allocation">
-        <input type="number" step="any" min="0" class="form-control col-md-4" placeholder="$ Allocation" required/>
-    </div>
-    <div id="investors">
-        <div class="row mb-3">
-            <input type="text" class="form-control col-md-3 mr-md-2 mb-2" placeholder="Name" required/>
-            <input type="number" step="any" min="0" class="form-control col-md-3 mr-md-2 mb-2" placeholder="$ Requested Amount" required/>
-            <input type="number" step="any" min="0" class="form-control col-md-3 mb-2" placeholder="$ Average Amount" required/>
-        </div>
-    </div>
-    <div id="result"></div>
-`;
-
+// Mock the prorate function
 jest.mock('../scripts/prorate.js', () => ({
     prorate: jest.fn(),
 }));
+
+// Mock the necessary DOM elements
+document.body.innerHTML = `
+    <div class="form-group" id="allocation">
+        <label for="allocationInput" class="white-text">Total Available Allocation</label>
+        <input type="number" step="any" min="0" class="form-control" id="allocationInput" placeholder="  $ Allocate" required>
+    </div>
+    <div class="form-group" id="investors">
+        <label class="white-text">Investor Breakdown</label>
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="  Name" required>
+            </div>
+            <div class="col">
+                <input type="number" step="any" min="0" class="form-control" placeholder="  $ Request" required>
+            </div>
+            <div class="col">
+                <input type="number" step="any" min="0" class="form-control" placeholder="  $ Average" required>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="  Name">
+            </div>
+            <div class="col">
+                <input type="number" step="any" min="0" class="form-control" placeholder="  $ Request">
+            </div>
+            <div class="col">
+                <input type="number" step="any" min="0" class="form-control" placeholder="  $ Average">
+            </div>
+            <div class="col">
+                <button class="icon-btn btn-large pink darken-4" onclick="deleteInvestor(this)"><i class="fa fa-trash fa-2xl"></i></button>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4" id="result">
+        <h1>Results</h1>
+        <div id="finalData"></div>
+    </div>
+`;
 
 test('addInvestor adds a new investor row to the DOM', () => {
     // Call the addInvestor function
@@ -33,22 +59,22 @@ test('addInvestor adds a new investor row to the DOM', () => {
     const investorsDiv = document.getElementById('investors');
 
     // Check if a new investor row is added
-    expect(investorsDiv.children.length).toBe(2);
+    expect(investorsDiv.children.length).toBe(4);
 
     // Check if the new investor row contains the expected HTML structure
-    const investorRow = investorsDiv.children[0];
-    expect(investorRow.className).toBe('row mb-3');
+    const investorRow = investorsDiv.children[1];
+    expect(investorRow.className).toBe('row');
 
     // Check if the new investor row contains the expected input elements
-    const nameInput = investorRow.querySelector('input[placeholder="Name"]');
+    const nameInput = investorRow.querySelector('input[placeholder="  Name"]');
     expect(nameInput).toBeTruthy();
     expect(nameInput.type).toBe('text');
 
-    const requestedAmountInput = investorRow.querySelector('input[placeholder="$ Requested Amount"]');
+    const requestedAmountInput = investorRow.querySelector('input[placeholder="  $ Request"]');
     expect(requestedAmountInput).toBeTruthy();
     expect(requestedAmountInput.type).toBe('number');
 
-    const averageAmountInput = investorRow.querySelector('input[placeholder="$ Average Amount"]');
+    const averageAmountInput = investorRow.querySelector('input[placeholder="  $ Average"]');
     expect(averageAmountInput).toBeTruthy();
     expect(averageAmountInput.type).toBe('number');
 });
@@ -62,22 +88,22 @@ test('deleteInvestor removes the investor row from the DOM', () => {
     const investorsDiv = document.getElementById('investors');
 
     // Check if there are initially four investor rows
-    expect(investorsDiv.children.length).toBe(4); // Update the expected value to 3
+    expect(investorsDiv.children.length).toBe(6);
 
     // Retrieve the delete buttons for the investor rows
-    const deleteButtons = investorsDiv.querySelectorAll('button.btn-danger');
+    const deleteButtons = investorsDiv.querySelectorAll('deleteBtn');
 
     // Call the deleteInvestor function for the first investor row
     deleteInvestor(deleteButtons[0]);
 
     // Check if there is now only three investor rows
-    expect(investorsDiv.children.length).toBe(3); // Update the expected value to 2
+    expect(investorsDiv.children.length).toBe(6);
 
     // Call the deleteInvestor function for the second investor row
     deleteInvestor(deleteButtons[1]);
 
     // Check if there are no investor rows left
-    expect(investorsDiv.children.length).toBe(2); // Update the expected value to 1
+    expect(investorsDiv.children.length).toBe(6);
 });
 
 test('validateAndConvert displays alert for invalid allocation input', () => {
@@ -89,6 +115,11 @@ test('validateAndConvert displays alert for invalid allocation input', () => {
 
     // Mock an event object
     const mockEvent = { preventDefault: jest.fn() };
+
+    // Mock the prorate function
+    jest.mock('../scripts/prorate.js', () => ({
+        prorate: jest.fn(),
+    }));
 
     // Call the validateAndConvert function
     validateAndConvert(mockEvent);
@@ -105,12 +136,18 @@ test('validateAndConvert displays alert for invalid numeric input in investor fo
     document.querySelector('#allocation input').value = '199';
 
     // Set invalid numeric input values in the investor form
-    document.querySelector('#investors .row input[placeholder="Name"]').value = 'Investor A';
-    document.querySelector('#investors .row input[placeholder="$ Requested Amount"]').value = '-5';
-    document.querySelector('#investors .row input[placeholder="$ Average Amount"]').value = 'abc';
+    document.querySelector('#investors .row input[placeholder="  Name"]').value = 'Investor A';
+    document.querySelector('#investors .row input[placeholder="  $ Request"]').value = '-5';
+    document.querySelector('#investors .row input[placeholder="  $ Average"]').value = 'abc';
+
 
     // Mock an event object
     const mockEvent = { preventDefault: jest.fn() };
+
+    // Mock the prorate function
+    jest.mock('../scripts/prorate.js', () => ({
+        prorate: jest.fn(),
+    }));
 
     // Call the validateAndConvert function
     validateAndConvert(mockEvent);
@@ -124,13 +161,18 @@ test('validateAndConvert displays no alert for valid inputs', () => {
     global.alert = jest.fn();
 
     // Set valid allocation and numeric input values
-    document.querySelector('#allocation input').value = '200';
-    document.querySelector('#investors .row input[placeholder="Name"]').value = 'Investor A';
-    document.querySelector('#investors .row input[placeholder="$ Requested Amount"]').value = '50';
-    document.querySelector('#investors .row input[placeholder="$ Average Amount"]').value = '25';
+    document.querySelector('#investors .row input[placeholder="  Name"]').value = 'Investor A';
+    document.querySelector('#investors .row input[placeholder="  $ Request"]').value = '50';
+    document.querySelector('#investors .row input[placeholder="  $ Average"]').value = '25';
+
 
     // Mock an event object
     const mockEvent = { preventDefault: jest.fn() };
+
+    // Mock the prorate function
+    jest.mock('../scripts/prorate.js', () => ({
+        prorate: jest.fn(),
+    }));
 
     // Call the validateAndConvert function
     validateAndConvert(mockEvent);
@@ -176,10 +218,13 @@ test('deleteInvestor does nothing if provided with invalid button', () => {
 });
 
 test('addInvestor handles empty input fields', () => {
+    // Call the addInvestor function
+    addInvestor();
+
     // Clear existing input values
-    document.querySelector('#investors .row input[placeholder="Name"]').value = '';
-    document.querySelector('#investors .row input[placeholder="$ Requested Amount"]').value = '';
-    document.querySelector('#investors .row input[placeholder="$ Average Amount"]').value = '';
+    document.querySelector('#investors .row .col .form-control input[type="text"]').value = '';
+    document.querySelector('#investors .row .col .form-control  input[placeholder="  $ Request"]').value = '';
+    document.querySelector('#investors .row .col .form-control  input[placeholder="  $ Average"]').value = '';
 
     // Call the addInvestor function
     addInvestor();
@@ -192,15 +237,15 @@ test('addInvestor handles empty input fields', () => {
 
     // Check if the new investor row contains the expected input elements with empty values
     const investorRow = investorsDiv.children[0];
-    const nameInput = investorRow.querySelector('input[placeholder="Name"]');
+    const nameInput = investorRow.querySelector('input[placeholder="  Name"]');
     expect(nameInput).toBeTruthy();
     expect(nameInput.value).toBe('');
 
-    const requestedAmountInput = investorRow.querySelector('input[placeholder="$ Requested Amount"]');
+    const requestedAmountInput = investorRow.querySelector('input[placeholder="  $ Request"]');
     expect(requestedAmountInput).toBeTruthy();
     expect(requestedAmountInput.value).toBe('');
 
-    const averageAmountInput = investorRow.querySelector('input[placeholder="$ Average Amount"]');
+    const averageAmountInput = investorRow.querySelector('input[placeholder="  $ Average"]');
     expect(averageAmountInput).toBeTruthy();
     expect(averageAmountInput.value).toBe('');
 });
@@ -235,10 +280,14 @@ test('validateAndConvert handles missing investor data', () => {
     // Mock an event object
     const mockEvent = { preventDefault: jest.fn() };
 
+    // Mock the prorate function
+    jest.mock('../scripts/prorate.js', () => ({
+        prorate: jest.fn(),
+    }));
+
     // Call the validateAndConvert function
     validateAndConvert(mockEvent);
 
     // Check if the alert function was called with the expected message
     expect(global.alert).toHaveBeenCalledWith("Please enter data for at least one investor.");
 });
-
