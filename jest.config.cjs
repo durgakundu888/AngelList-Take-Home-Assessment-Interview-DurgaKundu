16fd5111f6a4822e3e9b3b0b5644a4c4f165faa5
Durga Kundu <durgakundu888@gmail.com>
2023-11-21 07:00:20 -0800

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,6 @@
 // jest.config.js
 module.exports = {
     testEnvironment: 'node',
-    setupFiles: ['./setupJest.js'], // Adjust the path accordingly
-    // other Jest configurations...
+    setupFiles: ['./setupJest.js'],
   };
   
